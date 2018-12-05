918fba8a9bc68fc142b31340bd724fe9266aa74c
Daniel Chernyshev <droque123@gmail.com>
2018-12-05 15:20:49 +0300

    added build to git

---
+++ build/precache-manifest.36e04c04740466c0888d076fd91f16ab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1790b565df88c5e90bb",
+    "url": "/static/css/main.2b816b1b.chunk.css"
+  },
+  {
+    "revision": "b1790b565df88c5e90bb",
+    "url": "/static/js/main.b1790b56.chunk.js"
+  },
+  {
+    "revision": "cec31ec3307eaf5751c9",
+    "url": "/static/js/1.cec31ec3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "304240bbe0ffdf073edd82bb41f30d99",
+    "url": "/static/media/logo_white.304240bb.svg"
+  },
+  {
+    "revision": "0c46c9bd6900a6c4eaf7349bda096b40",
+    "url": "/static/media/kitkard_white.0c46c9bd.svg"
+  },
+  {
+    "revision": "477376d302911be898d715e94ca58a88",
+    "url": "/static/media/google.477376d3.png"
+  },
+  {
+    "revision": "70c06d745a82135a20b9450a2cd9a733",
+    "url": "/index.html"
+  }
+];
