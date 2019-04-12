6c684aba02a27bcfa96ee94ff33d78db6c870b52
Daniel Chernyshev <droque123@gmail.com>
2019-04-12 09:39:12 +0300

    Fixed AspectRatio

--- src/components/AspectRatio.js
+++ src/components/AspectRatio.js
@@ -1,15 +1,19 @@
 import React,  {Component} from 'react';
-import '../styles/AspectRatio.scss';
-
+import PropTypes from "prop-types";
+import {withStyles} from "@material-ui/core";
 
 class AspectRatio extends Component {
+    static propTypes = {
+        widthRatio: PropTypes.number.isRequired,
+        heightRatio: PropTypes.number.isRequired,
+    };
+
     state = {
         width: 0,
         height: 0
     };
 
     resize = () => {
-
         const {widthRatio, heightRatio} = this.props;
         const maxHeight = this.container.clientHeight - 20;
         const maxWidth = this.container.clientWidth - 20;
@@ -22,7 +26,7 @@ class AspectRatio extends Component {
             width = maxHeight * widthRatio / heightRatio;
         }
 
-        this.setState({ width, height });
+        this.setState({ width, height});
     };
 
     componentDidMount() {
@@ -36,13 +40,29 @@ class AspectRatio extends Component {
 
     render() {
 
-        return <div className='aspect-ratio'
-                    ref={ (container) => this.container = container}>
-            <div className="aspect-ratio-content" style={{width: this.state.width, height: this.state.height}}>
+        const {classes} = this.props;
+
+        return <div className={classes.container} ref={ (container) => this.container = container}>
+            <div className={classes.content} style={{width: this.state.width, height: this.state.height}}>
                 {this.props.children}
             </div>
         </div>;
     }
 }
 
-export default AspectRatio;
+
+
+const styles = (theme) => ({
+    container: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    content: {
+
+    }
+});
+
+export default withStyles(styles, {name: 'AspectRatio', withTheme: true})(AspectRatio);
