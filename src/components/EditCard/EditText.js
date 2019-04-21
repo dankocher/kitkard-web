7bff816fc5415f08f970db40e4e4b288116b6817
Daniel Chernyshev <droque123@gmail.com>
2019-04-21 12:44:42 +0300

    Edit name and description

--- src/components/EditCard/EditText.js
+++ src/components/EditCard/EditText.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-// import Touchable from 'react-native-platform-touchable';
-// import {ListItem} from "react-native-elements";
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
 import colors from "../../constants/colors";
 import translate from "../../translations";
@@ -21,20 +18,10 @@ class EditText extends React.Component {
     }
 
     editText = async() => {
-        /*await this.setState(previousState => {
-            return {is_private: !previousState.is_private}
-        });
-        this.card.is_private = this.state.is_private;
-        this.props.dispatch(saveCard(this.card));*/
         RightViewNavigator(
             <EditTextView username={this.props.user.username} __props={{...this.props, ...{onChangeValue: this.onChangeValue}}}/>
         );
-        // TODO: navigation
-        // this.props.navigation.navigate("NoAnimationNavigator", {
-        //     title: translate[this.props.language][this.props.type],
-        //     view: this.props.type,
-        //     cardname: this.props.cardname
-        // })
+
     };
 
     onChangeValue = (value) => {
@@ -45,47 +32,24 @@ class EditText extends React.Component {
         const color = colors[this.props.theme];
         const t = translate[this.props.language];
 
-        const card = this.props.cards[this.props.cardname];
-        console.log(this.props);
-
         return (
             <TouchableOpacity style={[styles.container, {borderColor: color.border }]}
                        activeOpacity={0.8}
                        onPress={() => this.editText()}>
                 <View style={styles.data}>
                     <Text style={[styles.name, { color: color.textSoft }]}>
-                        {card[this.props.type]}
+                        {t[this.props.type]}
                     </Text>
                     <Text style={[styles.value, { color: color.text}]}>
                         {this.state.value}
                     </Text>
                 </View>
-                {/*<ListItem*/}
-                    {/*style={{borderBottomWidth: 0}}*/}
-                    {/*title={t[this.props.type]}*/}
-                    {/*subtitle={card[this.props.type]}*/}
-                    {/*titleStyle={[styles.name, { color: color.textSoft }]}*/}
-                    {/*subtitleStyle={[styles.value, {color: color.text}]}*/}
-                    {/*containerStyle={{backgroundColor: 'transparent', borderBottomWidth: 0}}*/}
-                    {/*hideChevron*/}
-                    {/*noBorder={true}*/}
-                {/*/>*/}
             </TouchableOpacity>
         );
     }
 };
 export default EditText;
-/*
-const mapStateToProps = state => {
-    return {
-        user: state.user,
-        cards: state.cards,
-        language: state.language,
-        theme: state.theme
-    }
-};
-export default connect(mapStateToProps)(EditText)
-*/
+
 const styles = StyleSheet.create({
     container: {
         width: '100%',
